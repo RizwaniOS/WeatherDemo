bc1e4fdc61ffa4466846a46edaa356f9ca51f1c9
RizwaniOS <71660604+RizwaniOS@users.noreply.github.com>
2020-12-10 13:39:43 +0530

    Project created (#1)
    Co-authored-by: Mohammed Rizwan <rizu_93@yahoo.in>

---
+++ WeatherDemo/WeatherDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherDemo
+//
+//  Created by Mohammed Rizwan on 10/12/20.
+//  Copyright © 2020 Demo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
